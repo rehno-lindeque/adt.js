cb9c7e5e7ea751d2de79284d21b5e5b307bf963c
Rehno Lindeque <errantkid@gmail.com>
2012-03-26 14:45:39 -0700

    Start working on ADT constructors

--- src/adt.js
+++ src/adt.js
@@ -1,5 +1,26 @@
 (function() {
+  var 
+    // Eval an ADT in Array representation
+    _evalArrayRep = function() {
+    };
   ADT.eval = function(adt, f) {
+    // TODO
+  };
+  ADT.parse = function(text) {
+    // TODO
+  };
+  ADT.constructors = function() {
+    var
+      constructorNames = arguments,
+      makeConstructor = function(key) { return function() { [key].concat(arguments); }; },
+      adtConstructors = {},
+      i;
+    // Create ADT constructors
+    for (i = 0; i < constructorNames.length; ++i) {    
+      var name = constructorNames[i];
+      adtConstructors[name] = makeConstructor(name);
+    }
+    return adtConstructors;
   };
 })();
 
