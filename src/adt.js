b8eb32dfa8c3d75ebd07052650dcd5feb83a9354
Rehno Lindeque <errantkid@gmail.com>
2012-03-28 11:25:42 -0700

    Started working on an evaluator for adt's

--- src/adt.js
+++ src/adt.js
@@ -1,4 +1,55 @@
   // ADT constructor/evaluator api
+  var evaluator = (function() {
+    var 
+      evaluator = function() {
+        return evaluator.fn.eval.apply(evaluator, arguments);
+      },
+      init = function(adt) {
+        evaluator.adt = adt;
+        return evaluator;
+      };
+    
+    evaluator.fn = evaluator.prototype = {
+      eval: function(data) {
+        if (typeof(data) === 'string' || typeof(data) === 'number') {
+          if (typeof(adt[data]) === 'function')
+            return adt[data].call(adt, data);
+          else if (typeof(adt['_']) === 'function')
+            return adt['_'].call(adt, data);
+          else
+            return data;
+        }
+        if (Array.isArray(data) && data.length > 0) {
+          // Evaluate sub-trees
+          var
+            result = new Array(data.length),
+            key = '',
+            i;
+          for (i = 1; i < data.length; ++i) {
+            var subResult = evaluator.eval(data[i]);
+            if (Array.isArray(subResult))
+              if (subResult.length > 0 && (typeof(subResult[0]) === 'string' || typeof(subResult[0]) === 'number'))
+                key = key.concat(' '.concat(subResult[0]));
+            result[i] = subResult;
+          }
+          // TODO
+          console.log(String(data[0]).concat(key));
+          //return adt[data[0]].call(adt);
+          return result;
+        }
+        return data;
+      },
+      iterate: function(list) {
+        var 
+          result = [], 
+          i;
+        // TODO
+        return result;
+      }
+    }
+    return init;
+  })();
+
   adt.fn = adt.prototype = {
     init: function() {
       // Arguments to this function can be either constructor names (strings or 
@@ -19,22 +70,8 @@
               continue; // TODO: WARNING: expected function values in dispatch table
         else
           continue; // TODO: WARNING: unidentified argument passed to adt
-      } 
-      return this;
+      }
+      return function() { this.eval.apply(this, arguments); };
     },
-    parse: function(str) {
-      // TODO
-      /*var 
-        i = 0,
-        j = i;
-      for (; i < str.length + 1; ++i) {
-        if (str[i] === ' ' || i == str.length) {
-          if (str[j] === '(')
-          for (; j < i; ++j)
-            if (tokens[j] === '')
-          ++j;
-        }        
-      }*/
-    }
   };
 
