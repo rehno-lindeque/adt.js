e80d95e716b114867787403c6d1f9d508086018a
Rehno Lindeque <errantkid@gmail.com>
2012-05-13 14:37:22 -0700

    Simplified and expanded the serialize method using the new adt format to be implemented

--- src/serialize.js
+++ src/serialize.js
@@ -1,39 +1,44 @@
-  adt.serialize = function(){
+  adt.serialize = function(data){
     var 
-      serializeEval = adt.recursive(adt('serialized', {'_':
-        function() { 
-          var 
-            i, 
-            escapes = {
-              '\\': '\\\\',
-              '\"': '\\\"',
-              '\'': '\\\'',
-              '\t': '\\t',
-              '\r': '\\r',
-              '\n': '\\n',
-              ' ': '\\ ',
-              ',': '\\,',
-              '(': '\\(',
-              ')': '\\)',
-              '[': '\\[',
-              ']': '\\]',
-              '{': '\\{',
-              '}': '\\}'
-            },
-            str = escapeString(this._tag, escapes), 
-            data;
-          for (i = 0; i < arguments.length; ++i) {
-            // TODO: refactor (is deconstruct still necessary?)
-            data = adt.deconstruct(arguments[i]);
-            str += ' ' + (data.tag === 'string'? 
-              '"' + escapeString(data.args) + '"' : 
-              (data.tag === 'serialized'? 
-                "(" + data.args + ")" :
-                String(data.args)));
-          }
-          return this.serialized(str); 
-        }}
-      ));
-    return String(adt.deconstruct(serializeEval.apply(serializeEval, arguments)).args);
+      escapes = {
+        '\\': '\\\\',
+        '\"': '\\\"',
+        '\'': '\\\'',
+        '\t': '\\t',
+        '\r': '\\r',
+        '\n': '\\n',
+        ' ': '\\ ',
+        ',': '\\,',
+        '(': '\\(',
+        ')': '\\)',
+        '[': '\\[',
+        ']': '\\]',
+        '{': '\\{',
+        '}': '\\}'
+      },
+      SerializedADT = adt('SerializedADT').SerializedADT,
+      serializeEval = adt({
+        String: function(a) { return SerializedADT('"' + a + '"'); },
+        Number: function(a) { return SerializedADT(String(a)); },
+        Boolean: function(a) { return SerializedADT(a? 'True' : 'False'); },
+        // TODO: what about nested records, arrays and ADT's?
+        Array: function(a) { return SerializedADT('[' + String(a) + ']'); },
+        Arguments: function(a) { return this.Array([].slice.call(a)); },
+        // TODO: what about adt's nested inside the record...
+        Object: function(a) { return SerializedADT('"' + JSON.stringify(a) + '"'); },
+        SerializedADT: function(a) { return SerializedADT('(' + a + ')'); },
+        _: function() {
+          if (this._datatype !== 'ADT')
+            // Currently unsupported: RegExp, Null, Undefined, Math, JSON, Function, Error, Date
+            throw "Unsupported primitive type `" + this._datatype + "` in `adt.serialize`.";
+          var
+            i,
+            str = escapeString(this._tag, escapes);
+          for (i = 0; i < arguments.length; ++i)
+            str += ' ' + this(arguments[i])[0];
+          return SerializedADT(str);
+        }
+      });
+    return serializeEval(data);
   };
 
