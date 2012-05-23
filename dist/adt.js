0e44d444c3bda42229c4f8ad5259649b8f101ae1
Rehno Lindeque <errantkid@gmail.com>
2012-05-23 01:42:24 -0700

    Fixed a bug in the compose function The tags for the first interface in the chain weren't properly added and also needed to check if the adt is an interface and not just a function before adding its keys to the final interface

--- dist/adt.js
+++ dist/adt.js
@@ -200,14 +200,17 @@ var adt = (function() {
     if (a.length === 0)
       return adt();
     f = typeof a[0] === 'function'? a[0] : adt(a[0]);
-    tags = [];
     for (i = 1; i < a.length; ++i) {
       fi = typeof a[i] === 'function'? a[i] : adt(a[i]);
       f = (function(fi, f){ return function(){ return fi(f.apply(this, arguments)); }; })(fi, f);
-      for (key in fi)
-        if (key.length > 0 && key[0] !== '_')
-          tags.push(key);
     }
+    // Get all the tags of all of the interfaces
+    tags = [];
+    for (i = 0; i < a.length; ++i)
+      if (typeof a[i] === 'object' || isInterface(a[i]))
+        for (key in a[i])
+          if (key.length > 0 && key[0] !== '_')
+            tags.push(key);
     // Add all evaluators to the interface
     f._eval = f;
     for (i = 0; i < tags.length; ++i)
