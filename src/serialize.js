6b08d8a010b0cab1f20e5a1cef0e0fa67b25ea1b
Rehno Lindeque <errantkid@gmail.com>
2012-05-29 12:29:19 -0700

    Implement a code path for serializing adt's that aren't built-in types but have the same tag names

--- src/serialize.js
+++ src/serialize.js
@@ -16,29 +16,35 @@
         '{': '\\{',
         '}': '\\}'
       },
-      SerializedADT = adt('SerializedADT').SerializedADT,
+      //SerializedADT = adt('SerializedADT').SerializedADT,
+      serializeTagStruct = function(tag, args) {
+        var
+            i,
+            str = escapeString(tag, escapes),
+            parens;
+          for (i = 0; i < args.length; ++i) {
+            parens = isADT(args[i]) && args[i].length > 0;
+            str += ' ' + (parens? '(' : '') + serializeEval(args[i]) + (parens? ')' : '');
+          }
+          return str;
+      },
+      // TODO: shorten this by using `compose`?
       serializeEval = adt({
-        String: function(a) { return SerializedADT('"' + a + '"'); },
-        Number: function(a) { return SerializedADT(String(a)); },
-        Boolean: function(a) { return SerializedADT(a? 'True' : 'False'); },
+        String: function(a) { return this._datatype === 'ADT'? serializeTagStruct('String', arguments) : '"' + a + '"'; },
+        Number: function(a) { return this._datatype === 'ADT'? serializeTagStruct('Number', arguments) : String(a); },
+        Boolean: function(a) { return this._datatype === 'ADT'? serializeTagStruct('Boolean', arguments) : (a? 'True' : 'False'); },
         // TODO: what about nested records, arrays and ADT's?
-        Array: function(a) { return SerializedADT('[' + String(a) + ']'); },
-        Arguments: function(a) { return this.Array([].slice.call(a, 0)); },
+        Array: function(a) { return this._datatype === 'ADT'? serializeTagStruct('Array', arguments) : '[' + String(a) + ']'; },
+        Arguments: function(a) { return this._datatype === 'ADT'? serializeTagStruct('Arguments', arguments) : this([].slice.call(a, 0)); },
         // TODO: what about adt's nested inside the record...
-        Object: function(a) { return SerializedADT('"' + JSON.stringify(a) + '"'); },
-        SerializedADT: function(a) { return SerializedADT('(' + a + ')'); },
+        Object: function(a) { return this._datatype === 'ADT'? serializeTagStruct('Object', arguments) : '"' + JSON.stringify(a) + '"'; },
+        //SerializedADT: function(a) { return '(' + a + ')'; },
         _: function() {
           if (this._datatype !== 'ADT')
             // Currently unsupported: RegExp, Null, Undefined, Math, JSON, Function, Error, Date
             throw "Unsupported primitive type `" + this._datatype + "` in `adt.serialize`.";
-          var
-            i,
-            str = escapeString(this._tag, escapes);
-          for (i = 0; i < arguments.length; ++i)
-            str += ' ' + this(arguments[i])[0];
-          return SerializedADT(str);
+          return serializeTagStruct(this._tag, arguments);
         }
       });
-    return serializeEval(data)[0];
+    return serializeEval(data);
   };
-
