a4dd18d715d2e9b198f1edf343771bec5690ffd6
Rehno Lindeque <errantkid@gmail.com>
2012-06-03 16:36:53 -0700

    Fix a bug in the recursive function (it should use the new `_tag` attribute)

--- src/recursive.js
+++ src/recursive.js
@@ -3,13 +3,13 @@
         var i, results = [], subResult;
         if (!isADT(data))
           return f(data);
-        for (i = 1; i < data.length; ++i) {
+        for (i = 0; i < data.length; ++i) {
           subResult = recurse(data[i]);
           if (typeof subResult !== 'undefined')
             results.push(subResult);
         }
         // TODO: Take into account pattern matching requirements...
-        return f(construct(data[0], results));
+        return f(construct(data._tag, results));
     };
     // Assign all the methods in the interface to the recursive interface too
     // TODO: But shouldn't these methods also run recursively?
