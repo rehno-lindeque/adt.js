7c52805120093379c22e874bd49c053ea2ef6c8d
Rehno Lindeque <errantkid@gmail.com>
2012-07-22 18:53:19 +0200

    Remove comment about escaping characters since this is already done afterwards

--- dist/adt.js
+++ dist/adt.js
@@ -338,7 +338,6 @@ var adt = (function() {
             if ((searchIndex - i) & 1 === 1) // There is an even number of slashes
               return { head: str.slice(0, searchIndex + 1), tail: str.slice(searchIndex + 1) };
             else // There is an odd number of slashes, so continue searching
-              // TODO: Properly serialize the escaped character
               break;
           }
         searchIndex += 1;
