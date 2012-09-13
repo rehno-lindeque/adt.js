613dab1910282c571a70f1c8054aea762f159c2c
Rehno Lindeque <errantkid@gmail.com>
2012-09-13 10:42:37 +0200

    Add numeric (unicode) escapes to the deserialize method

--- src/core.js
+++ src/core.js
@@ -50,12 +50,19 @@
         result = '',
         replacement,
         escapes = escapes || {
-          '\\': '\\\\',
-          '\"': '\\\"',
-          '\'': '\\\'',
-          '\t': '\\t',
-          '\r': '\\r',
-          '\n': '\\n'
+          // Single-character escape codes (JavaScript -> Haskell)
+          '\0': '\\0',    // null character
+          //'\a': '\\a',  // alert            (n/a in JavaScript)
+          '\b': '\\b',    // backspace
+          '\f': '\\f',    // form feed
+          '\n': '\\n',    // newline (line feed)
+          '\r': '\\r',    // carriage return
+          '\t': '\\t',    // horizontal tab
+          '\v': '\\v',    // vertical tab
+          '\"': '\\\"',   // double quote
+          //'\&': '\\&',  // empty string     (n/a in JavaScript)
+          '\'': '\\\'',   // single quote
+          '\\': '\\\\'    // backslash
         };
       for (i = 0; i < str.length; ++i) {
         replacement = escapes[str[i]];
@@ -68,21 +75,107 @@
         i,
         result = '',
         escapes = {
-          '\\': '\\',
-          '\"': '\"',
-          '\'': '\'',
-          't': '\t',
-          'r': '\r',
-          'n': '\n'
+          // Single-character escape codes (Haskell -> JavaScript)
+          //'0': '\0',    // null character   (handled by numeric escape codes)
+          'a': '',        // alert            (n/a in javaScript)
+          'b': '\b',      // backspace
+          'f': '\f',      // form feed
+          'n': '\n',      // newline (line feed)
+          'r': '\r',      // carriage return
+          't': '\t',      // horizontal tab
+          'v': '\v',      // vertical tab
+          '\"': '\"',     // double quote
+          '&': '',        // empty string
+          '\'': '\'',     // single quote
+          '\\': '\\'      // backslash
         };
+        /* ASCII control code abbreviations (Haskell -> JavaScript)
+        \NUL  U+0000  null character
+        \SOH  U+0001  start of heading
+        \STX  U+0002  start of text
+        \ETX  U+0003  end of text
+        \EOT  U+0004  end of transmission
+        \ENQ  U+0005  enquiry
+        \ACK  U+0006  acknowledge
+        \BEL  U+0007  bell
+        \BS U+0008  backspace
+        \HT U+0009  horizontal tab
+        \LF U+000A  line feed (newline)
+        \VT U+000B  vertical tab
+        \FF U+000C  form feed
+        \CR U+000D  carriage return
+        \SO U+000E  shift out
+        \SI U+000F  shift in
+        \DLE  U+0010  data link escape
+        \DC1  U+0011  device control 1
+        \DC2  U+0012  device control 2
+        \DC3  U+0013  device control 3
+        \DC4  U+0014  device control 4
+        \NAK  U+0015  negative acknowledge
+        \SYN  U+0016  synchronous idle
+        \ETB  U+0017  end of transmission block
+        \CAN  U+0018  cancel
+        \EM U+0019  end of medium
+        \SUB  U+001A  substitute
+        \ESC  U+001B  escape
+        \FS U+001C  file separator
+        \GS U+001D  group separator
+        \RS U+001E  record separator
+        \US U+001F  unit separator
+        \SP U+0020  space
+        \DEL  U+007F  delete
+        */
+        /* Control-with-character escapes (Haskell -> JavaScript)
+        \^@ U+0000  null character
+        \^A through \^Z U+0001 through U+001A control codes
+        \^[ U+001B  escape
+        \^\ U+001C  file separator
+        \^] U+001D  group separator
+        \^^ U+001E  record separator
+        \^_ U+001F  unit separator
+        */
       for (i = 0; i < str.length - 1; ++i) {
         if (str[i] !== '\\')
           result += str[i];
         else {
-          var replacement = escapes[str[i + 1]];
-          result += (replacement == null? str[i + 1] : replacement);
+          var 
+            s = str[i + 1],
+            replacement = escapes[s],
+            numStr = null,
+            radix = null;
+          if (replacement != null) {
+            result += replacement;
+            ++i;
+            continue;
+          }
+          // Parse numeric escapes
+          if (s >= '0' && s <= '9') {
+            numStr = (/[0-9]*/).exec(str.slice(i + 1))[0];
+            radix = 10; 
+          } else if (s == 'x') {
+            numStr = (/[0-9a-f]*/i).exec(str.slice(i + 2))[0];
+            radix = 16;
+          } else if (s == 'o') {
+            numStr = (/[0-7]*/).exec(str.slice(i + 2))[0];
+            radix = 8;
+          }
+          if (numStr != null && numStr.length > 0) {
+            var 
+              num = 0,
+              j;
+            for (j = 0; j < numStr.length; ++j) {
+              num *= radix;
+              num += parseInt(numStr[j], radix);
+            }
+            result += String.fromCharCode(num);
+            console.log(numStr,numStr.length);
+            i += numStr.length + (s == 'x' || s == 'o'? 1 : 0);
+            continue;
+          }
+          // Direct single-character escape
+          result += str[i + 1];
           ++i;
-        }          
+        }
       }
       // Add the last character if it wasn't escaped
       return i === str.length - 1? result + str[str.length - 1] : result;
