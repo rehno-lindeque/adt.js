f31f24d8cf80a0620bdb448ac2bcb7764013bd25
Rehno Lindeque <errantkid@gmail.com>
2012-09-16 10:56:13 +0200

    Improved recursion for regular functions on data values

--- dist/adt.js
+++ dist/adt.js
@@ -314,22 +314,27 @@ var adt = (function() {
     return f;
   };
   adt.recursive = function(f) {
+    if (typeof f !== 'function')
+      throw "Expected a function or ADT interface in adt.recursive"
+    var self = isInterface(f)? f : adt({_: f});
+
     var recurse = function (data) {
         var i, results = [], subResult;
-        if (!isADT(data))
-          return f(data);
+        if (!isADT(data)) {
+          return self(data);
+        }
         for (i = 0; i < data.length; ++i) {
           subResult = recurse(data[i]);
           //if (typeof subResult !== 'undefined')
           results.push(subResult);
         }
         // TODO: Take into account pattern matching requirements...
-        return f(construct(data._tag, results));
+        return self(construct(data._tag, results));
     };
     // Assign all the methods in the interface to the recursive interface too
     // TODO: But shouldn't these methods also run recursively?
-    for (var key in f)
-      recurse[key] = f[key];
+    for (var key in self)
+      recurse[key] = self[key];
     return recurse;
   };
   // Create ADT's from an object's own property names (both enumerable + non-enumerable)
