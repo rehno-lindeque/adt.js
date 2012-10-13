629115a116915e0fd15edeffaf05f9cceb8b5ac4
Rehno Lindeque <errantkid@gmail.com>
2012-10-13 15:39:32 +0200

    Fix deserialize for empty strings

--- dist/adt3.js
+++ dist/adt3.js
@@ -586,9 +586,9 @@ var adt = (function() {
         if (searchIndex === -1)
           throw "No closing quotation mark was found for the string starting with " + str.slice(0, Math.min(5, str.length)) + "...";
         // Check if there's an odd number of escape characters before the quotation mark character
-        for (i = searchIndex - 1; i > 0; --i)
+        for (i = searchIndex - 1; i >= 0; --i)
           if (str[i] !== '\\') {
-            if ((searchIndex - i) & 1 === 1) // There is an even number of slashes
+            if ((searchIndex - i) & 1 === 1) // There is an even number of slashes (or none)
               return { head: str.slice(0, searchIndex + 1), tail: str.slice(searchIndex + 1) };
             else // There is an odd number of slashes, so continue searching
               break;
