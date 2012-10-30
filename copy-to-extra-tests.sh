f5f5f9b31dfdf243e18b016d9bace67fc0d7d75b
Rehno Lindeque <errantkid@gmail.com>
2012-10-30 13:33:15 +0200

    Add script command to copy adt.js to adt-svg extra

--- copy-to-extra-tests.sh
+++ copy-to-extra-tests.sh
@@ -1,6 +1,7 @@
 cp dist/adt.js extra/adt-typecheck.js/test/lib/adt.js
 cp dist/adt.js extra/adt-util.js/test/lib/adt.js
 cp dist/adt.js extra/adt-html.js/test/lib/adt.js
+cp dist/adt.js extra/adt-svg.js/test/lib/adt.js
 cp dist/adt.js extra/supersimple-editor.js/test/lib/adt.js
 cp extra/adt-html.js/dist/adt-html.js extra/supersimple-editor.js/test/lib/adt-html.js
 
